4d5f669a40d0485855432f54a4ca2811bd6dd6b9
Al Zee <z@alz.ee>
2025-08-21 17:54:32 +0800

    nth

--- run.sh
+++ run.sh
@@ -8,30 +8,31 @@
 
 ############### Main Part ###############
 
-echo Load ash...
-unzip ash.zip
-mv ash-master ~/.ash
-~/.ash/a.sh -L
-rm ash.zip
+echo Install packages...
+pkgs=$(< pkgs)
+sudo dnf install -y $pkgs
 
-echo Install tools...
-sudo dnf install -y flex bison httpd readline-devel
+for i in pkgs:
+do
+    echo Install... $i
+    echo Try to get $i from repo...
+    sudo dnf install -y $i
 
-echo Building screen...
-pushd screen/
-. build_screen.sh
-popd
+    # If install from repo failed
+    if [ $? != 0]; then
+        echo CAN NOT install $i from repo...
+        echo Try to build from source...
+        pushd $i 2> /dev/null || echo $i dir not found ; continue
+        echo Building $i...
+        . build_$i.sh
+        popd
+    fi
 
-###
-echo Building php...
-pushd php/
-. build_php.sh
-popd
-
-###
-echo Building postgresql...
-pushd postgresql/
-. build_postgresql.sh
-popd
-
-sudo systemctl enable --now httpd
+    # Post install setup
+    if [ -f $i/setup_$i.sh ]; then
+        pushd $i 2> /dev/null || echo $i dir not found ; continue
+        echo Running post setup for $i...
+        . setup_$i.sh
+        popd
+    fi
+done
