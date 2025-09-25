8c4b5befd997de00f4201167886976f9bd80cb2a
Al Zee <z@alz.ee>
2025-09-25 11:59:12 +0800

    nth

--- php/setup.sh
+++ php/setup.sh
@@ -2,11 +2,13 @@
 #
 # vim:ft=sh
 
+sudo mkdir -p /usr/local/etc/php-fpm.d/
 sudo cp www.conf /usr/local/etc/php-fpm.d/
 sudo cp php-fpm.service /usr/lib/systemd/system/
 sudo cp php-fpm.conf /usr/local/etc/
 sudo chmod 644 /usr/local/etc/php-fpm.conf
-sudo mkdir /var/log/php-fpm/
+sudo mkdir -p /var/log/php-fpm/
 
+sudo systemctl daemon-reload
 sudo systemctl enable --now php-fpm
 #sudo setfacl -m u:apache:rw /run/php-fpm/www.sock
