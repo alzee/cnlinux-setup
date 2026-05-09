8d68ef95630d05c7ca8339c9818a41a3f6643f60
Al Zee <z@alz.ee>
2026-05-09 11:48:47 +0800

    nth

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 # set -x
 ############### Variables ###############
-pkg=dnf
+pm=dnf
 pdir=$(realpath $(dirname $0))  # use absolute dir in case change/pushd dir
 
 ############### Functions ###############
@@ -32,12 +32,24 @@ log() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a "$pdir/log/$logfile"
 }
 
+post_setup(){
+    local pkg
+    pkg="$1"
+    # Post install setup
+    if [ -f "$pkg"/setup.sh ]; then
+        pushd "$pkg" 2> /dev/null || { echo "$pkg" dir not found ; continue; }
+        echo Running post setup for "$pkg"...
+        . setup.sh
+        popd
+    fi
+}
+
 ############### Main Part ###############
 which apt &> /dev/null
 if [ $? = 0 ]; then
     echo Found apt
-    pkg=apt
-    sudo $pkg update
+    pm=apt
+    sudo $pm update
 fi
 
 mkdir -p log
@@ -50,29 +62,31 @@ do
     echo Install... $i
     echo Try to get $i from repo...
     # TODO log
-    sudo $pkg install -y $i
+    sudo $pm install -y $i
 
     if [ $? = 0 ]; then
-            log "$i installed from repo" i_success
+        log "$i installed from repo" i_success
     else
         echo CAN NOT install $i from repo...
-        if [ -f $i/build.sh ]; then
-            echo Try to build from source...
-            pushd $i 2> /dev/null || { echo $i dir not found ; continue; }
-            echo Building $i...
-            . build.sh
-            [ $? = 0] && log "$i build success" b_success
-            popd
-        else
-            log "$i install failed..." i_err
-        fi
+        log "$i install failed..." i_err
     fi
 
-    # Post install setup
-    if [ -f $i/setup.sh ]; then
+    post_setup "$i"
+done
+
+echo Build packages...
+builds=$(< builds)
+
+for i in $builds
+do
+    if [ -f $i/build.sh ]; then
+        echo Try to build from source...
         pushd $i 2> /dev/null || { echo $i dir not found ; continue; }
-        echo Running post setup for $i...
-        . setup.sh
+        echo Building $i...
+        . build.sh
+        [ $? = 0] && log "$i build success" b_success
         popd
     fi
+
+    post_setup "$i"
 done
